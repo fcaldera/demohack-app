d5be7c876ccd9ede5f803567fb2b4810bea2b5f4
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-09-21 19:38:55 +0300

    chore: make spacer handle vertical border

--- src/components/common/spacer.js
+++ src/components/common/spacer.js
@@ -2,7 +2,12 @@ import styled from 'styled-components'
 import {Box} from "@primer/components";
 
 export const Spacer = styled(Box)`
-  border-bottom: 1px solid ${props => props.theme.colors.gray[3]};
+  border-bottom: ${({theme, variant }) => variant === 'vertical' ? 'none' : `1px solid ${theme.colors.gray[3]}`};
+  border-right: ${({theme, variant }) => variant === 'vertical' ? `1px solid ${theme.colors.gray[3]}` : 'none' };
 `;
+
+Spacer.defaultProps = {
+  variant: 'horizontal'
+}
 
 export default Spacer
