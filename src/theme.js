022d4496545817a6f0ee3fd52314ff667073f784
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-09-21 19:38:12 +0300

    feat: add button theme

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,50 @@
 import { theme as primer } from "@primer/components";
+import { rgba } from 'polished';
 
 
 const colors = {
   ...primer.colors,
-  blue: '#374AD3'
+  blue: '#374AD3',
+  pink: '#E1A2A3'
 }
+
+const buttons = {
+  outline: {
+    color: colors.blue,
+    backgroundColor: 'transparent',
+    border: `1px solid ${colors.blue}`, //rgba(255,255,255, .75),
+    '&:hover': {
+      color: colors.white,
+      backgroundColor: colors.blue,
+      borderColor: colors.blue,
+    },
+  },
+  [`no-border`]: {
+    backgroundColor: 'transparent',
+    border: 'none',
+  },
+  success: {
+    color: 'white',
+    backgroundColor: colors.blue,
+    borderColor: colors.blue,
+    '&:hover': {
+      color: 'white',
+      backgroundColor: rgba(colors.blue, 0.95),
+      borderColor: colors.blue,
+    },
+  },
+  active: {
+    color: 'black',
+    backgroundColor: colors.white,
+    borderColor: colors.white,
+    '&:hover': {
+      color: colors.white,
+      backgroundColor: rgba(colors.black, 0.95),
+      borderColor: colors.blue,
+    },
+  },
+};
+
 
 const Theme = {
   ...primer,
@@ -18,6 +58,7 @@ const Theme = {
     normal: 400,
     bold: 700
   },
+  buttons
 }
 
 export default Theme
