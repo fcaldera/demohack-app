76fb8474bda293058df7d02b3ad13b48f5b5c9ed
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2019-09-21 14:09:39 +0300

    chore: update color from design

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,14 @@
 import { theme as primer } from "@primer/components";
 
+
+const colors = {
+  ...primer.colors,
+  blue: '#374AD3'
+}
+
 const Theme = {
   ...primer,
+  colors,
   fonts: {
     normal: "Manjari, Arial, sans-serif",
     mono: "Manjari, Arial, sans-serif",
